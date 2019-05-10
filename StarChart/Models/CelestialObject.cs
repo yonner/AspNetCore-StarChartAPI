b3b8808c33d0d786773964e2eb2b0e528b948f0d
Jason Wroe <Jason.Wroe@ivanti.com>
2019-05-10 14:44:57 +0100

    Step 3

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+//using System.
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get;set;}
+        public int? OrbitingObject { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Satellities { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+
+    }
+}
